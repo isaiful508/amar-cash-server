ee771c0325cf71018dd39fe212485f51d19571f2
isaiful508 <isaiful.islam508@gmail.com>
2024-07-16 00:07:38 +0600

    Alhamdulillah basic set up done

---
+++ index.js
@@ -0,0 +1,57 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+require('dotenv').config();
+const bcrypt = require('bcryptjs');
+
+const port = process.env.port || 5000;
+const corsOptions = {
+    origin: ['http://localhost:5173'
+    ],
+    credentials: true,
+    optionSuccessStatus: 200,
+  }
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.bhtyeej.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Salam from Amar Cash server')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Amar Cash is running on port ${port}`);
+  })
